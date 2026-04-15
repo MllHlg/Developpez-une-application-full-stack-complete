556a0cf8d2565aca4ecd743521d3751c671a6ffe
MllHlg <maelle772001@gmail.com>
2026-04-15 18:50:44 +0200

    Ajout de la barre de navigation responsive.

--- front/src/app/shared/components/nav-bar/nav-bar.ts
+++ front/src/app/shared/components/nav-bar/nav-bar.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIcon, MatMenuModule, CommonModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
 export class NavBar {
   @Input() fullInfos!: boolean;
+
+  public isMenuOpen = false;
 }
